e442e95dcd9bdb5993e91229bdb80af4e897a37e
John Wilfred Montilde <john@wingaru.com.au>
2023-02-26 23:23:34 +0800

    [Project 1] - Improve code implementation

--- projects/email_sender.py
+++ projects/email_sender.py
@@ -4,23 +4,24 @@ import smtplib
 
 from config import EMAIL_SENDER, EMAIL_APP_PASSWORD, TEMP_EMAIL_RECEIVER
 
-email_sender = EMAIL_SENDER
-email_app_password = EMAIL_APP_PASSWORD
-email_receiver = TEMP_EMAIL_RECEIVER
+def send_email(email_sender, email_app_password, email_receiver):
 
-subject = "Just a test email sender"
-body = """
-If you receive this, then you're lucky
-"""
+    subject = "Just a test email sender"
+    body = """
+    If you receive this, then this is your lucky day!
+    """
 
-em = EmailMessage()
-em['From'] = EMAIL_SENDER
-em['To'] = email_receiver
-em['subject'] = subject
-em.set_content(body)
+    em = EmailMessage()
+    em['From'] = EMAIL_SENDER
+    em['To'] = email_receiver
+    em['subject'] = subject
+    em.set_content(body)
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender, email_app_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_app_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+if __name__ == "__main__":
+    send_email(EMAIL_SENDER, EMAIL_APP_PASSWORD, TEMP_EMAIL_RECEIVER)
